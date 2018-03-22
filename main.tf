c1f8278e298aaf6c1ebc1d795e9346149faf1a05
Sneha Paramasivan <Sneha.Paramasivan@team.telstra.com>
2018-03-22 14:30:10 +1100

    Corrected instance shape

--- main.tf
+++ main.tf
@@ -28,12 +28,14 @@ resource "opc_compute_ip_network" "cla-npintocm-wloc-mmf001" {
 resource "opc_compute_storage_volume" "storage-1" {
 	name                = "storage-1"
 	size                = "30"
+	image_list          = "/oracle/public/OL_6.8_UEKR4_x86_64"
 }
 
 resource "opc_compute_storage_volume" "storage-1-Additional" {
 	name                = "storage-1-Additional"
 	size                = "30"
 	bootable            = true
+	image_list          = "/oracle/public/OL_6.8_UEKR4_x86_64"
 }
 
 ### COMPUTE ###
