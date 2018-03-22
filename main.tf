4ecde3034fe524f7e69359de43bf6d1c61d13842
Sneha Paramasivan <Sneha.Paramasivan@team.telstra.com>
2018-03-22 13:47:41 +1100

    Added Network Exchange resource

--- main.tf
+++ main.tf
@@ -7,6 +7,13 @@ provider "opc" {
    endpoint            = "${var.apiEndpoint}"
 }
 
+### IP Network : Network Exchange ###
+resource "opc_compute_ip_network_exchange" "cla-npintocm-ipx001" {
+	name                = "cla-npintocm-ipx001"
+	description			= "Prod OCM -IP Exchange No 1 - IP Exchange for Clayton NpIntocm non prod network"
+	tags 				= [ "cla-npintocm-nonprod" ]
+}
+
 ### STORAGE ###
 ###############
 resource "opc_compute_storage_volume" "app-volume1" {
@@ -23,4 +30,12 @@ resource "opc_compute_instance" "lxapp14001" {
 	name                = "lxapp14001"
 	label               = "CAT RMS app server (+additional 300GB storage)"
 	shape               = "OC3M"
+	storage {
+		index 			= 1
+		volume          = "${opc_compute_storage_volume.storage-1.name}"
+	}
+	
+#	ssh_keys            = ["${opc_compute_ssh_key.ocsk-public-key1.name}"]
+	boot_order          = [ 1 ]
 }
+	
