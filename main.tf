05498708b531ca2d6c704e1025373c9e39d9b9e7
Sneha Paramasivan <Sneha.Paramasivan@team.telstra.com>
2018-03-22 14:26:52 +1100

    Corrected instance shape

--- main.tf
+++ main.tf
@@ -28,14 +28,12 @@ resource "opc_compute_ip_network" "cla-npintocm-wloc-mmf001" {
 resource "opc_compute_storage_volume" "storage-1" {
 	name                = "storage-1"
 	size                = "30"
-	image_list          = "/oracle/public/OL_6.8_UEKR4_x86_64"
 }
 
 resource "opc_compute_storage_volume" "storage-1-Additional" {
 	name                = "storage-1-Additional"
 	size                = "30"
 	bootable            = true
-	image_list          = "/oracle/public/OL_6.8_UEKR4_x86_64"
 }
 
 ### COMPUTE ###
@@ -43,7 +41,7 @@ resource "opc_compute_storage_volume" "storage-1-Additional" {
 resource "opc_compute_instance" "lxapp14001" {
 	name                = "lxapp14001"
 	label               = "CAT RMS app server (+additional 300GB storage)"
-	shape               = "OC3"
+	shape               = "oc3"
 	storage {
 		index 			= 1
 		volume          = "${opc_compute_storage_volume.storage-1.name}"
