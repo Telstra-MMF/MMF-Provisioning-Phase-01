0ec29411fbc084678430ef1c10c9f50392accf92
Sneha Paramasivan <Sneha.Paramasivan@team.telstra.com>
2018-03-26 10:08:22 +1100

    Add compute2 file to test multiple files apply

---
+++ compute2.tf
@@ -0,0 +1,24 @@
+### COMPUTE ###
+###############
+resource "opc_compute_instance" "lxapp14002" {
+	name                = "lxapp14002"
+	label               = "CAT RMS app server 2"
+	shape               = "oc3"
+	storage {
+		index 			= 1
+		volume          = "${opc_compute_storage_volume.storage-1.name}"
+	}
+	
+	networking_info {
+		index             = 0
+		shared_network    = false
+		vnic              = "vnic001"
+		ip_network        = "cla-npintocm-wloc-mmf001"
+		ip_address        = "10.195.77.12"
+		dns               = ["lxapp14001.ocmint.corp.telstra.com"]
+	}
+	
+#	ssh_keys            = ["${opc_compute_ssh_key.ocsk-public-key1.name}"]
+	boot_order          = [ 1 ]
+}
+	
