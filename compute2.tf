9a759567d4c051985865e57d5b588fcfd5be2580
Sneha Paramasivan <Sneha.Paramasivan@team.telstra.com>
2018-03-27 14:14:26 +1100

    Storage 1 - Bootable

--- compute2.tf
+++ compute2.tf
@@ -3,6 +3,7 @@
 resource "opc_compute_storage_volume" "storage-1" {
 	name                = "storage-1"
 	size                = "30"
+	bootable 			= true
 	image_list          = "/oracle/public/OL_6.8_UEKR4_x86_64"
 }
 
