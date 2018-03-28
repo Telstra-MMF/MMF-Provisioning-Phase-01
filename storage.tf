188419638c85784564ffc2ff6290dd86930f09bf
Sneha Paramasivan <Sneha.Paramasivan@team.telstra.com>
2018-03-29 10:14:50 +1100

    Network config

--- storage.tf
+++
@@ -1,8 +0,0 @@
-### STORAGE ###
-###############
-resource "opc_compute_storage_volume" "storage-1" {
-	name                = "storage-1"
-	size                = "30"
-	bootable 			= true
-	image_list          = "/oracle/public/OL_6.8_UEKR4_x86_64"
-}
