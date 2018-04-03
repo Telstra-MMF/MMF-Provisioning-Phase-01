8cbb83351ba111c33b31ece4f3d774c2e3f1316e
Sneha Paramasivan <Sneha.Paramasivan@team.telstra.com>
2018-04-03 15:16:20 +1000

    prevent_destroy

--- instance1.tf
+++ instance1.tf
@@ -1,11 +1,13 @@
 ### STORAGE ###
 ###############
 resource "opc_compute_storage_volume" "storage-1" {
-	prevent_destroy		= true
 	name                = "storage-1"
 	size                = "30"
 	bootable 			= true
 	image_list          = "/oracle/public/OL_6.8_UEKR4_x86_64"
+	lifecycle {
+         prevent_destroy = true
+     }
 }
 
 ### COMPUTE ###
