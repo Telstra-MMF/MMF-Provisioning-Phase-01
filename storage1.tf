567a112915b5745583a7925eb81d43e96f7e48f8
Sneha Paramasivan <Sneha.Paramasivan@team.telstra.com>
2018-04-04 11:05:08 +1000

    storage1-imagelistentry3

--- storage1.tf
+++ storage1.tf
@@ -2,5 +2,9 @@ resource "opc_compute_storage_volume" "storage-1" {
 	name                = "storage-1"
 	size                = "30"
 	bootable 			= true
-	image_list          = "/oracle/public/OL_6.8_UEKR4_x86_64"     
+	image_list          = "/oracle/public/OL_6.8_UEKR4_x86_64"    
+	image_list_entry 	= 3
+/*	lifecycle {
+         create_before_destroy = true
+	}*/
 }
--- storage2.tf
+++
@@ -1,7 +0,0 @@
-resource "opc_compute_storage_volume" "storage-2" {
-	name                = "storage-2"
-	size                = "30"
-	bootable 			= true
-	image_list          = "/oracle/public/OL_6.8_UEKR4_x86_64"    
-	image_list_entry 	= 2
-}
