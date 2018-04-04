a6db5b3ede364e715b948e04aa6b9dc20ec84225
Sneha Paramasivan <Sneha.Paramasivan@team.telstra.com>
2018-04-04 11:02:00 +1000

    storage2

--- storage2.tf
+++ storage2.tf
@@ -2,5 +2,6 @@ resource "opc_compute_storage_volume" "storage-2" {
 	name                = "storage-2"
 	size                = "30"
 	bootable 			= true
-	image_list          = "/oracle/public/OL_6.8_UEKR4_x86_64"     
+	image_list          = "/oracle/public/OL_6.8_UEKR4_x86_64"    
+	image_list_enttry 	= 2
 }
